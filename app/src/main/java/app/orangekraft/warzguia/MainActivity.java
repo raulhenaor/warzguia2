ac88fa6f41fb94fc6145ed68630b8e2347056168
raul jesus henao roa <raulhenaor@gmail.com>
2020-10-27 20:47:19 -0500

    Fragment 1, Fragment 2, Menu Principal, Home con tabs

--- app/src/main/java/app/orangekraft/warzguia/MainActivity.java
+++ app/src/main/java/app/orangekraft/warzguia/MainActivity.java
@@ -1,14 +1,61 @@
 package app.orangekraft.warzguia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+import app.orangekraft.warzguia.Controlador.PagerController;
 
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+    TabItem tab1, tab2, tab3;
+    ViewPager viewPager;
+    PagerController pagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tabLayout);
+        viewPager = findViewById(R.id.viewpager);
+        tab1 = findViewById(R.id.tabmapas);
+        tab2 = findViewById(R.id.tabarmas);
+
+        pagerAdapter = new PagerController(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pagerAdapter);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                if (tab.getPosition()==0){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+                if (tab.getPosition()==1){
+                    pagerAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+
+
+
+
+
     }
 }
