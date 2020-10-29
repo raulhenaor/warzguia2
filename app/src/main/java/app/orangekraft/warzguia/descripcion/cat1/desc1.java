88bc7ef2549ee03db77d9af24fcfd61b4f45205d
raul jesus henao roa <raulhenaor@gmail.com>
2020-10-29 10:50:11 -0500

    Descripcion rdy

--- app/src/main/java/app/orangekraft/warzguia/descripcion/cat1/desc1.java
+++ app/src/main/java/app/orangekraft/warzguia/descripcion/cat1/desc1.java
@@ -1,16 +1,34 @@
 package app.orangekraft.warzguia.descripcion.cat1;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import app.orangekraft.warzguia.R;
 
 public class desc1 extends AppCompatActivity {
+    ImageView atras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_desc1);
+
+        atras = (ImageView) findViewById(R.id.btnc1a1d1);
+
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
+
+    public void atras (View view) {
+       onBackPressed();
+    }
+
 }
