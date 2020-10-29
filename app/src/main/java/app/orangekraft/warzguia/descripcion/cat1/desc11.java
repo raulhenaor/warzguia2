fb44ab1b5bd2f7270659beda8fe3f68838c55f00
raul jesus henao roa <raulhenaor@gmail.com>
2020-10-29 15:47:29 -0500

    FusilAsalto OK Descripcion1 OK Funcionan links bien

---
+++ app/src/main/java/app/orangekraft/warzguia/descripcion/cat1/desc11.java
@@ -0,0 +1,27 @@
+package app.orangekraft.warzguia.descripcion.cat1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import app.orangekraft.warzguia.R;
+
+public class desc11 extends AppCompatActivity {
+    ImageView atras;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desc11);
+
+        atras = (ImageView) findViewById(R.id.btnc1a1d1);
+
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
--- app/src/main/java/app/orangekraft/warzguia/ui/escopeta.java
+++ app/src/main/java/app/orangekraft/warzguia/ui/escopeta.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import app.orangekraft.warzguia.R;
+
 public class escopeta extends AppCompatActivity {
 
     @Override
