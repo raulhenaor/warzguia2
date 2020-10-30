f0e345018abcc47a804c364207289b4bb375a313
raul jesus henao roa <raulhenaor@gmail.com>
2020-10-30 11:45:46 -0500

    FusilPrecision ok Listo

---
+++ app/src/main/java/app/orangekraft/warzguia/descripcion/cat6/descc6l0.java
@@ -0,0 +1,29 @@
+package app.orangekraft.warzguia.descripcion.cat6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import app.orangekraft.warzguia.R;
+
+public class descc6l0 extends AppCompatActivity {
+    ImageView atras;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descc6l0);
+
+        atras = (ImageView)findViewById(R.id.btnc1a1d1);
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+
+    }
+}
