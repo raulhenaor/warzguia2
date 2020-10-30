ee8ee5ae704665d64dc70591ec81917295e301b2
raul jesus henao roa <raulhenaor@gmail.com>
2020-10-30 10:12:40 -0500

    Escopetas OK

---
+++ app/src/main/java/app/orangekraft/warzguia/descripcion/cat3/descc3l4.java
@@ -0,0 +1,26 @@
+package app.orangekraft.warzguia.descripcion.cat3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import app.orangekraft.warzguia.R;
+
+public class descc3l4 extends AppCompatActivity {
+    ImageView atras;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_descc3l4);
+
+        atras = (ImageView)findViewById(R.id.btnc1a1d1);
+        atras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
--- app/src/main/java/app/orangekraft/warzguia/ui/escopeta.java
+++ app/src/main/java/app/orangekraft/warzguia/ui/escopeta.java
@@ -1,7 +1,9 @@
 package app.orangekraft.warzguia.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -9,9 +11,16 @@ import android.widget.ImageView;
 import app.orangekraft.warzguia.R;
 
 import app.orangekraft.warzguia.R;
+import app.orangekraft.warzguia.descripcion.cat2.descc2l0;
+import app.orangekraft.warzguia.descripcion.cat2.descc2l1;
+import app.orangekraft.warzguia.descripcion.cat2.descc2l2;
+import app.orangekraft.warzguia.descripcion.cat2.descc2l3;
+import app.orangekraft.warzguia.descripcion.cat2.descc2l4;
+import app.orangekraft.warzguia.descripcion.cat2.descc2l5;
 
 public class escopeta extends AppCompatActivity {
     ImageView IvRegresar;
+    CardView card0, card1,card2, card3, card4, card5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,61 @@ public class escopeta extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        card0 = (CardView) findViewById(R.id.Cardc3l0);
+        card0.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i= new Intent(escopeta.this, descc2l0.class);
+                startActivity(i);
+            }
+        });
+
+        card1 = (CardView) findViewById(R.id.Cardc3l1);
+        card1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i= new Intent(escopeta.this, descc2l1.class);
+                startActivity(i);
+            }
+        });
+
+        card2 = (CardView) findViewById(R.id.Cardc3l2);
+        card2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i= new Intent(escopeta.this, descc2l2.class);
+                startActivity(i);
+            }
+        });
+
+        card3 = (CardView) findViewById(R.id.Cardc3l3);
+        card3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i= new Intent(escopeta.this, descc2l3.class);
+                startActivity(i);
+            }
+        });
+
+        card4 = (CardView) findViewById(R.id.Cardc3l4);
+        card4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i= new Intent(escopeta.this, descc2l4.class);
+                startActivity(i);
+            }
+        });
+
+        card5 = (CardView) findViewById(R.id.Cardc3l5);
+        card5.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent i= new Intent(escopeta.this, descc2l5.class);
+                startActivity(i);
+            }
+        });
+
 
     }
 }
